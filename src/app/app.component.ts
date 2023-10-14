1475f1210ba99a915201c52c8ae84b8352b43cf8
DanielHabenicht <daniel-habenicht@outlook.de>
2023-10-14 20:23:34 +0000

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DefaultService } from 'src/api';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ulanzi-ui';
+  public stats: any;
+
+  constructor(private apiService: DefaultService){
+    this.apiService.statsGet().subscribe(stats => {
+      this.stats = stats
+    })
+  }
 }
